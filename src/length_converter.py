78c1a3eb6c14ea03fed9b7f89fa8b95fa3c466e2
iWaheeb <waheebbasalem@gmail.com>
2024-11-11 17:54:41 +0300

    added test convert to centimeter

--- src/length_converter.py
+++ src/length_converter.py
@@ -1,23 +1,23 @@
-def convert_to_millimeter(value: float, unit: str):
+def convert_to_millimeter(from_unit: str, value: float):
     return value
 
-def convert_to_centimeter(value: float, unit: str):
+def convert_to_centimeter(from_unit: str, value: float):
     return value
 
-def convert_to_meter(value: float, unit: str):
+def convert_to_meter(from_unit: str, value: float):
     return value
 
-def convert_to_kilometer(value: float, unit: str):
+def convert_to_kilometer(from_unit: str, value: float):
     return value
 
-def convert_to_inch(value: float, unit: str):
+def convert_to_inch(from_unit: str, value: float):
     return value
 
-def convert_to_foot(value: float, unit: str):
+def convert_to_foot(from_unit: str, value: float):
     return value
 
-def convert_to_yard(value: float, unit: str):
+def convert_to_yard(from_unit: str, value: float):
     return value
 
-def convert_to_mile(value: float, unit: str):
+def convert_to_mile(from_unit: str, value: float):
     return value
--- tests/test_convert_length.py
+++ tests/test_convert_length.py
@@ -1,11 +1,24 @@
-from src.length_converter import convert_to_millimeter
+from src.length_converter import (
+    convert_to_millimeter,
+    convert_to_centimeter,
+    convert_to_meter
+)
 
 
 def test_convert_to_millimeter():
-    assert convert_to_millimeter(15, "cm") == 150
-    assert convert_to_millimeter(3, "m") == 3000
-    assert convert_to_millimeter(4.5, "km") == 4500000
-    assert convert_to_millimeter(24, "in") == 609.6
-    assert convert_to_millimeter(7, "ft") == 2133.6
-    assert convert_to_millimeter(1.3, "yd") == 1188.72
-    assert convert_to_millimeter(2, "mi") == 3218688
+    assert convert_to_millimeter("cm", 15) == 150
+    assert convert_to_millimeter("m", 3) == 3000
+    assert convert_to_millimeter("km", 4.5) == 4500000
+    assert convert_to_millimeter("in", 24) == 609.6
+    assert convert_to_millimeter("ft", 7) == 2133.6
+    assert convert_to_millimeter("yd", 1.3) == 1188.72
+    assert convert_to_millimeter("mi", 2) == 3218688
+
+def test_convert_to_centimeter():
+    assert convert_to_centimeter("mm", 30) == 3
+    assert convert_to_centimeter("m", 3) == 300
+    assert convert_to_centimeter("km", 4.5) == 450000
+    assert convert_to_centimeter("in", 27) == 68.58
+    assert convert_to_centimeter("ft", 9) == 274.32
+    assert convert_to_centimeter("yd", 1.2) == 109.728
+    assert convert_to_centimeter("mi", 1.8) == 289681.92
